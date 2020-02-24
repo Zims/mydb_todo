e89200ed0b70473f363c887ab6e570d9d72b2f1f
Zigmars Vilcans <zims@inbox.lv>
2020-02-24 15:52:31 +0200

    db_helper

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -1,1 +1,18 @@
-import 'package:flutter/material.dart';
+import 'Note.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper _databaseHelper; //singleton
+  static Database _database;
+
+  String noteTable = 'note_table';
+  String colID = 'id';
+  String colTitle = 'title';
+  String colDescription = 'description';
+  String colPriority = 'priority';
+  String colDate  = 'date';
+
+}
