e3fbe1fa15ceca6e678c630f0614fa3e95f301ee
Zigmars Vilcans <zims@inbox.lv>
2020-02-26 11:20:02 +0200

    note_list

--- lib/screens/note_list.dart
+++ lib/screens/note_list.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import '../database_helper.dart';
+import '../Note.dart';
+import 'note_details.dart';
+import 'package:sqflite/sqflite.dart';
 
 class NoteList extends StatefulWidget {
+  DatabaseHelper databaseHelper = DatabaseHelper();
+  List<Note> noteList;
+  int count = 0;
+
   @override
   _NoteListState createState() => _NoteListState();
 }
@@ -8,8 +17,30 @@ class NoteList extends StatefulWidget {
 class _NoteListState extends State<NoteList> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('LCO ToDo'),
+          backgroundColor: Colors.purple,
+        ),
+        body: Container(),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.purple,
+          child: Icon(
+            Icons.add,
+          ),
+          onPressed: () {
+            navigateToDetail(Note('', '', 2), 'Add Note');
+          },
+        ));
+  }
+
+  void navigateToDetail(Note note, String title) async {
+    bool result =
+        await Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return NoteDetail(note, title);
+    }));
+    if (result == true) {
+      // updateListView();
+    }
   }
 }
